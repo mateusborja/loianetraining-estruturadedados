75a9c968f17a9f9f47ef31bcf1732089c2ce9f1b
Mateus Borja <mateus.borja@gmail.com>
2020-12-01 11:03:34 -0400

    aula 02 vetores

---
+++ src/com/mateusborja/estruturadados/aula02/Vetor.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.estruturadados.aula02;
+
+public class Vetor {
+	
+	private String elementos[];
+	
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String [capacidade];
+		
+	}
+
+}
