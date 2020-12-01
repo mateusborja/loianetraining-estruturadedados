8ce8887ac9ad79f57513d9685718f9fbadc4c0b9
Mateus Borja <mateus.borja@gmail.com>
2020-12-01 11:03:58 -0400

    aula 02 classe teste vetores

---
+++ src/com/mateusborja/estruturadados/aula02/ClasseTeste.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020.  | All rights reserved
+ * @author Mateus Borja // UX Designer • 3D Artist • Developer
+ * at www.mateusborja.life
+ */
+
+package com.mateusborja.estruturadados.aula02;
+
+public class ClasseTeste {
+	
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+	
+
+
+		
+		int i = 0;
+		i++;
+
+		
+		
+		
+	}
+
+}
