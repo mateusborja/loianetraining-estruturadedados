abc88d1659607393ebf2a95d6f078aef9f274f57
Mateus Borja <mateus.borja@gmail.com>
2020-12-04 11:43:09 -0400

    aula 02 classe vetor com tamanho e vetor de elementos

--- src/com/mateusborja/estruturadados/aula02/Vetor.java
+++ src/com/mateusborja/estruturadados/aula02/Vetor.java
@@ -3,17 +3,23 @@
  * @author Mateus Borja // UX Designer • 3D Artist • Developer
  * at www.mateusborja.life
  */
-
+//aula 02 vetor classe def
 package com.mateusborja.estruturadados.aula02;
 
 public class Vetor {
 	
 	private String elementos[];
+	private int tamanho;
 	
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String [capacidade];
+		this.tamanho = 0;
 		
+	}
+	
+	public int getTamanho() {
+		return tamanho;
 	}
 
 }
